ee27ec1fde8158c6d6d95c518f23faac4e90ef76
TVerloop <t93.verloop@gmail.com>
2018-02-16 12:16:36 +0100

    feat: automatic installation script.
     - script will copy .ros-root to ~/.ros-root on the pepper robot
     - script will set ROS_IP, ROS_MASTER_URI and NAO_IP according
    to given options.

--- setup_ros1_pepper.bash
+++ setup_ros1_pepper.bash
@@ -3,5 +3,8 @@
 export PYTHONHOME="/home/nao/.ros-root/Python-2.7.13"
 export PATH="${PYTHONHOME}/bin:${PATH}"
 export LD_LIBRARY_PATH="/home/nao/.ros-root/ros1_dependencies/lib:${PYTHONHOME}/lib:${LD_LIBRARY_PATH}"
+export ROS_IP=$(ip addr show wlan0 | grep -Po '(?<= inet )([0-9]{1,3}.){3}[0-9]{1,3}')
+export NAO_IP=$ROS_IP
+export ROS_MASTER_URI=http://127.0.0.1:11311
 
 source /home/nao/.ros-root/ros1_inst/setup.bash
